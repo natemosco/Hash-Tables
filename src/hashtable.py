27834d8983345a761989b3d144a8ce2b7316486d
Nathaniel Mosco <natemosco@gmail.com>
2020-04-07 19:08:40 -0400

    terrible single commit :-/ almost MVP

--- src/hashtable.py
+++ src/hashtable.py
@@ -9,6 +9,35 @@ class LinkedPair:
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        return f"<{self.key}, {self.value}>"
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, key, value):
+        new_node = LinkedPair(key, value)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+    def search(self, key):
+        current = self.head
+        found = False
+
+        while current and found is False:
+            if current.key == key:
+                found = True
+            else:
+                current = current.next
+        if current is None:
+            return False
+        return current
+
 
 class HashTable:
     '''
@@ -46,15 +75,34 @@ class HashTable:
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
         # Part 1: Hash collisions should be handled with an error warning. (Think about and
         # investigate the impact this will have on the tests)
-
         # Part 2: Change this so that hash collisions are handled with Linked List Chaining.
-
         Fill this in.
         '''
-        pass
+        # Hashmod the key to find the bucket
+        index = self._hash_mod(key)
+
+        if self.storage[index] == None:
+            self.storage[index] = LinkedList()
+            self.storage[index].insert(key, value)
+        else:
+            node = self.storage[index].search(key)
+            if node:
+                node.value = value
+            else:
+                self.storage[index].insert(key, value)
+        ''' # Check if a pair already exists in the bucket
+        pair = self.storage[bucket]
+        if pair is not None:
+            # If so, overwrite the key/value and throw  a warning
+            if pair.key != key:
+                print("Warning: Overwriting value")
+                pair.key = key
+            pair.value = value
+        else:
+            # If not, Creat a new LinkedPair and place it in the bucket
+        '''
 
     def remove(self, key):
         '''
@@ -64,7 +112,14 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        # Get the index from hashmod
+        index = self._hash_mod(key)
+        # Check if a pair exists in the bucket with matchin keys
+        if self.storage[index] is not None and self.storage[index].key == key:
+            # If so, return the value
+            self.storage[index] = None
+        else:
+            raise ValueError("Key is not found.")
 
     def retrieve(self, key):
         '''
@@ -74,7 +129,16 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        # Get the index from hashmod
+        index = self._hash_mod(key)
+
+        # Check if a pair exists in the bucket with matchin keys
+        if self.storage[index] is not None and self.storage[index].key == key:
+            # If so, return the value
+            return self.storage[index].value
+        else:
+            # Else return None
+            return None
 
     def resize(self):
         '''
@@ -83,7 +147,17 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        self.capacity = self.capacity * 2
+        new_hash_table = HashTable(self.capacity)
+
+        for i in range(len(self.storage)):
+            if self.storage[i] is not None:
+                current_node = self.storage[i].head
+                while current_node:
+                    new_hash_table.insert(current_node.key, current_node.value)
+                    current_node = current_node.next
+        self.storage = new_hash_table.storage
+        new_hash_table = None
 
 
 if __name__ == "__main__":
